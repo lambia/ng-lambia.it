a3c547e137a9d1eb70ec91d1be928d1dec97ef1e
Luca <luca@lambia.it>
2020-10-02 23:00:38 +0200

    naming e refactoring

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -16,69 +16,75 @@ export class MatrixComponent implements OnInit {
   stamps = STAMPS; //M3
   filteredStamps = null; //M3
 
-  storeGroups = ["colors", "things", "categories"];
-  store = {
+  itemsFields = ["colors", "things", "categories"];
+  items = {
     colors: [
-      { id: 1, name: "Red", links: { things: [1,2,3], categories: [1,2,3] } },
-      { id: 2, name: "White", links: { things: [4,5], categories: [3] } },
-      { id: 3, name: "Green", links: { things: [6,7], categories: [1,2] } },
+      { id: 1, name: "Red", fields: { things: [1,2,3], categories: [1,2,3] } },
+      { id: 2, name: "White", fields: { things: [4,5], categories: [3] } },
+      { id: 3, name: "Green", fields: { things: [6,7], categories: [1,2] } },
     ],
     things: [
-      { id: 1, name: "Blood", links: { colors: [1], categories: [2] } },
-      { id: 2, name: "Fire", links: { colors: [1], categories: [3] } },
-      { id: 3, name: "Cherries", links: { colors: [1], categories: [1] } },
-      { id: 4, name: "Sun", links: { colors: [2], categories: [3] } },
-      { id: 5, name: "Snow", links: { colors: [2], categories: [3] } },
-      { id: 6, name: "Grass", links: { colors: [3], categories: [2] } },
-      { id: 7, name: "Avocado", links: { colors: [3], categories: [1,2] } },
+      { id: 1, name: "Blood", fields: { colors: [1], categories: [2] } },
+      { id: 2, name: "Fire", fields: { colors: [1], categories: [3] } },
+      { id: 3, name: "Cherries", fields: { colors: [1], categories: [1] } },
+      { id: 4, name: "Sun", fields: { colors: [2], categories: [3] } },
+      { id: 5, name: "Snow", fields: { colors: [2], categories: [3] } },
+      { id: 6, name: "Grass", fields: { colors: [3], categories: [2] } },
+      { id: 7, name: "Avocado", fields: { colors: [3], categories: [1,2] } },
     ],
     categories: [
-      { id: 1, name: "Fruits", links: { colors: [1,3], things: [3,7] } },
-      { id: 2, name: "Organic", links: { colors: [1,3], things: [1,3,6,7] } },
-      { id: 3, name: "Physics", links: { colors: [1,2], things: [1,4,5] } },
+      { id: 1, name: "Fruits", fields: { colors: [1,3], things: [3,7] } },
+      { id: 2, name: "Organic", fields: { colors: [1,3], things: [1,3,6,7] } },
+      { id: 3, name: "Physics", fields: { colors: [1,2], things: [1,4,5] } },
     ]
   };
-  selected = { group: null, value: null };
-  
-  initData() {
-    this.storeGroups.map(group => {
-      this.store[group].map(item=>{
-        item.selected = false;
-      });
-    });
-  }
+  selected = { field: null, value: null };
 
-  //ToDo: naming dello store
   //ToDo: typing e model
-  //ToDo: includere l'init con params?=null ??
-  //prendi this.store[property] con id = value
-  groupFiltering(data: object, groups: string[], group: string, value: string): void {
-      //per ogni gruppo esistente
-      groups.map(storeGroup=>{
-        data[storeGroup].map(storeItem=>{
-          //Se si tratta del gruppo del filtro
-          if(storeGroup==group) {
-            //Evidenzia solo il selezionato (un filtro per gruppo)
-            storeItem.selected = (storeItem.id==value);
-          } else {
-            //Altrimenti, evidenzia quelle che corrispondono al filtro
-            storeItem.selected = storeItem.links[group].indexOf(value)>=0;
-          }
+  //Imposta "selected" per gli oggetti con il valore "value" nella proprietà id dell'oggetto "field"
+  fieldFiltering(data: object, fields: string[], field?: string, value?: string): void {
+      //Se non sono stati forniti gruppo e valore per cui filtrare
+      if(!field || !value) {
+        //Per ogni gruppo esistente
+        fields.map(itemsField=>{
+          //Per ogni elemento
+          data[itemsField].map(item=>{
+            item.selected = true; //cfg.AllSelectedOnLoad
+          });
         });
-      });
+      //Se sono stati forniti gruppo e valore per cui filtrare
+      } else {
+        //per ogni gruppo esistente
+        fields.map(itemsField=>{
+          //Per ogni elemento
+          data[itemsField].map(item=>{
+            //Se si tratta del gruppo del filtro
+            if(itemsField==field) {
+              //Evidenzia solo il selezionato (un filtro per gruppo)
+              item.selected = (item.id==value);
+            } else {
+              //Altrimenti, evidenzia quelle che corrispondono al filtro
+              item.selected = item.fields[field].indexOf(value)>=0;
+            }
+          });
+        });
 
+      }
   }
 
-  onFilter(group: string, value: string): void {
-    if(this.selected.group==group && this.selected.value == value) {
-      //Se il valore era già selezionato, deseleziona
-      this.selected = { group: null, value: null };
-      this.initData();
-    } else {
-      //Se il valore non era selezionato, filtra
-      this.selected = { group: group, value: value };
-      this.groupFiltering(this.store, this.storeGroups, group, value);
+  onFilter(field: string, value: string): void {
+    //Se il valore era già selezionato, deseleziona
+    if(this.selected.field==field && this.selected.value==value) {
+      field = value = null;
     }
+    
+    this.selected = { field, value };
+    this.fieldFiltering(this.items, this.itemsFields, field, value);
+  }
+
+  initData(): void {    
+    this.selected = { field: null, value: null };
+    this.fieldFiltering(this.items, this.itemsFields);
   }
 
   constructor() { }
